8d6a3df86bbd64a732386e3a7747c4153779c637
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-09-24 03:41:56 -0700

    finished chapter 8, on to 9! power of data()

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 var d3 = require('d3');
 
-console.log(d3, "bitches");
+var body = d3.select('body');
+
+var dataset = [ 5, 10, 15, 20, 25 ];
+
+// body.selectAll('p')
+// .data(dataset)
+// .enter()
+// .append('p')
+// .text(function(d) { return d; })
+// .style('padding', '10px')
+// .style('color', 'white')
+// .style("background-color", function(d){
+//   if ( d % 2 === 0 ){
+//     return 'blue';
+//   } else {
+//     return 'green';
+//   }
+// });
+
+body.selectAll('div')
+    .data(dataset)
+    .enter()
+    .append('div')
+    .attr('class', 'bar')
+    .style('height', function(d) {
+      return d * 5 + 'px';
+    });
